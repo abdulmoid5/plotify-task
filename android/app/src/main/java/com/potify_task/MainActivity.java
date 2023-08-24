725c0881aca6e208d1033a04eb4128f1f2e5b4a8
Abdul Moied Babar <abdulmoidbabar@gmail.com>
2023-08-25 03:32:10 +0500

    Change name (#1)

--- android/app/src/main/java/com/potify_task/MainActivity.java
+++ android/app/src/main/java/com/potify_task/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "potify_task";
+    return "Plotify Task";
   }
 
   /**
