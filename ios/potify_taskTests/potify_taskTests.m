5d046c0c805e78120201848dc534c6608618766a
Abdul Moied Babar <abdulmoidbabar@gmail.com>
2023-08-25 04:02:29 +0500

    Revert "Change name (#1)" (#3)

--- ios/potify_taskTests/potify_taskTests.m
+++ ios/potify_taskTests/potify_taskTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PlotifyTaskTests : XCTestCase
+@interface potify_taskTests : XCTestCase
 
 @end
 
-@implementation PlotifyTaskTests
+@implementation potify_taskTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
